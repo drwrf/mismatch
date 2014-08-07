3b1b7faf7a789467bb15a9f4e9b9a312c8701c80
frwrd <hi@drwrf.com>
2014-08-06 22:54:16 -0400

    Add some basic tests for the entity.

--- lib/Mismatch/Entity.php
+++ lib/Mismatch/Entity.php
@@ -61,14 +61,37 @@ class Entity
     /**
      * Changes a value or set of values on the entity.
      *
-     * @param   string  $data
+     * @param   string  $name
      * @param   mixed   $value
      * @return  $this
      */
     public function write($name, $value)
     {
+        // Mark the field as unchanged if the value has not deviated
+        // from the original value that the entity holds. We get slightly
+        // smarter change tracking this way.
+        if (array_key_exists($name, $this->data) && $this->data[$name] === $value) {
+            unset($this->changes[$name]);
+            return;
+        }
+
         $this->changes[$name] = $value;
 
         return $this;
+    }
+
+    /**
+     * Returns whether or not a value has changed.
+     *
+     * Interestingly, this will return true so long as the value
+     * has been written to, regardless of whether or not the value
+     * has already changed.
+     *
+     * @param   string  $name
+     * @return  bool
+     */
+    public function changed($name)
+    {
+        return array_key_exists($name, $this->changes);
     }
 }
