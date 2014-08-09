c085ac8fe423ec7642aae5d233ecc0a2a8abbdc6
frwrd <hi@drwrf.com>
2014-08-08 21:28:54 -0500

    Add query methods as statics on the model.

--- lib/Mismatch/ORM.php
+++ lib/Mismatch/ORM.php
@@ -22,8 +22,28 @@ trait ORM
         };
 
         // The connection the mode will use to talk to the database.
-        $m['connection'] = function ($m) {
+        $m['connection'] = $m->factory(function ($m) {
             return ORM\Connector::connect($m['credentials']);
-        };
+        });
+
+        // The query builder used for SELECTs
+        $m['query'] = $m->factory(function($m) {
+            return new ORM\Query($m['connection'], $m['pk']);
+        });
+    }
+
+    /**
+     * Proxy to the query builder, so that all of its methods are
+     * exposed as static methods on the class.
+     *
+     * @param  string  $method
+     * @param  array   $args
+     * @return mixed
+     */
+    public static function __callStatic($method, array $args)
+    {
+        $query = Metadata::get(get_called_class())['query'];
+
+        return call_user_func_array([$query, $method], $args);
     }
 }
